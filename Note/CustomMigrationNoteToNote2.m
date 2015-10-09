5799f6ad74e319ae865a0d62d99434cb9800f66d
Александр Карцев <sanekkartsev@gmail.com>
2015-10-09 14:07:40 +0300

    something work

--- Note/CustomMigrationNoteToNote2.m
+++ Note/CustomMigrationNoteToNote2.m
@@ -10,62 +10,20 @@
 
 @implementation CustomMigrationNoteToNote2
 
-// called once, before the start of the migration
--(BOOL)beginEntityMapping:(NSEntityMapping *)mapping
-                  manager:(NSMigrationManager *)manager
-                    error:(NSError *__autoreleasing *)error
-{
-    // create a dictionary to store State entities
-    NSMutableDictionary *userInfo = [NSMutableDictionary dictionary];
-    userInfo[@"states"] = [NSMutableDictionary dictionary];
-    
-    // give the dictionary to the migration manager
-    [manager setUserInfo:userInfo];
-    
-    return YES;
-}
-
 -(BOOL)createDestinationInstancesForSourceInstance:(NSManagedObject *)sInstance
                                      entityMapping:(NSEntityMapping *)mapping
                                            manager:(NSMigrationManager *)manager
                                              error:(NSError *__autoreleasing *)error
 {
-    // destination managed object context and entity name
-    NSManagedObjectContext *destinationManagedObjectContext
-    = [manager destinationContext];
-    NSString *destinationEntityName
-    = [mapping destinationEntityName];
-    
-    // create the Address entity in the destination model
-    NSManagedObject *dInstance
-    = [NSEntityDescription insertNewObjectForEntityForName:destinationEntityName
-                                    inManagedObjectContext:destinationManagedObjectContext];
-    // set the non-normalized attributes
-    [dInstance setValue:[sInstance valueForKey:@"street"] forKeyPath:@"street"];
-    [dInstance setValue:[sInstance valueForKey:@"city"] forKeyPath:@"city"];
-    
-    // lookup its state
-    NSString *stateName = [sInstance valueForKey:@"state"];
-    NSMutableDictionary *states = [manager userInfo][@"states"];
-    NSManagedObject *state = states[stateName];
-    
-    // create and store a new State entity if not found
-    if (!state) {
-        state
-        = [NSEntityDescription insertNewObjectForEntityForName:@"State"
-                                        inManagedObjectContext:destinationManagedObjectContext];
-        [state setValue:stateName forKeyPath:@"name"];
-        states[stateName] = state;
-    }
-    
-    // set the State relationship on the Address entity
-    [dInstance setValue:state forKeyPath:@"state"];
-    
-    // associate the source and destination Address entities
-    [manager associateSourceInstance:sInstance
-             withDestinationInstance:dInstance
-                    forEntityMapping:mapping];
-    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    NSData *image = [sInstance valueForKey:@"image"];
+    [dateFormatter setDateFormat:@"yyyyMMddHHmmss"];
+    NSString *newDate = [dateFormatter stringFromDate:[sInstance valueForKey:@"date"]];
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask, YES);
+    NSString *documentsPath = [paths objectAtIndex:0];
+    NSString *newName = [NSString stringWithFormat:@"%@.png",newDate];
+    NSString *temp = [documentsPath stringByAppendingPathComponent:newName];
+    [image writeToFile:temp atomically:YES];
     return YES;
 }
 
--- Note/DataManager.h
+++ Note/DataManager.h
@@ -8,6 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
+#import "CustomMigrationNoteToNote2.h"
 
 @interface DataManager : NSObject
 
